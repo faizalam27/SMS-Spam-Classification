e07b0339f50cac5501f6d8ac3cb73a7fcd02a04b
faizalam27 <134140156+faizalam27@users.noreply.github.com>
2024-01-14 01:17:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import string
+from scipy.sparse import hstack
+import numpy as np
+
+ps = PorterStemmer()
+
+cv = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title('IS HE/SHE A SPAM PERSON?')
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]  # here we are cloning
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]  # here we are cloning
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+input_sms = st.text_area('Enter the message')
+
+if st.button('Predict'):
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = cv.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("CAREFUL: HE/SHE IS SPAM")
+    else:
+        st.header("CONGRATS: HE/SHE IS GOOD")
